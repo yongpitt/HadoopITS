6420ab1881660c7ad14f0689e29b745e33f7b6e7
Yong Li <mysec@126.com>
2013-06-19 20:35:02 -0400

    Literature Study

---
+++ Materials/11182010 Interacting with Kinect - Concepts and Research Opportunities/Interacting with Kinect - Concepts and Research Opportunities - MIPSInternal_files/index.php
@@ -0,0 +1,10 @@
+
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '/internalwiki/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Monobook.js */
+/* Any JavaScript here will be loaded for users using the MonoBook skin */
